12c4346fd2028c480f89afdc11dd75027ded6ee6
John Saba <john.thomas.saba@gmail.com>
2014-01-25 16:02:22 -0600

    player moves and changes with checkpoints

---
+++ DestructionOfRa.spritebuilder/Source/Checkpoint.h
@@ -0,0 +1,16 @@
+//
+//  Checkpoint.h
+//  DestructionOfRa
+//
+//  Created by John Saba on 1/25/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNodeColor.h"
+
+@interface Checkpoint : CCNodeColor
+
+@property (assign) CGFloat dx;
+@property (assign) CGFloat dy;
+
+@end
---
+++ DestructionOfRa.spritebuilder/Source/Checkpoint.m
@@ -0,0 +1,13 @@
+//
+//  Checkpoint.m
+//  DestructionOfRa
+//
+//  Created by John Saba on 1/25/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Checkpoint.h"
+
+@implementation Checkpoint
+
+@end
